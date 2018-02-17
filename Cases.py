39b11202d58a8b94de0d70710180fa5f4b8fe039
DampirFX <for_work_mail@list.ru>
2018-02-17 19:15:04 +0700

    Add some test

--- Cases.py
+++ Cases.py
@@ -2,7 +2,9 @@ from ChangeProfile_Request import *
 import allure
 
 
-###################################################################
+#############################################################################
+#Проверка запроса ChangeProfile
+#############################################################################
 def wrong_request():
     allure.description("Check wrong request")
     data = {
@@ -13,4 +15,124 @@ def wrong_request():
             }
     FinishResult = falseurl(data)
     return FinishResult
-###################################################################
+#############################################################################
+def no_body_request():
+    allure.description("No body on request")
+    data = {}
+    FinishResult = falseurl(data)
+    return FinishResult
+#############################################################################
+def request_without_name_and_platform():
+    allure.description("No parameters name and platform on request")
+    data = {"tradingProfile":{}}
+    FinishResult = falseurl(data)
+    return FinishResult
+#############################################################################
+def request_without_name():
+    allure.description("No parameter name on request")
+    data = {
+        "tradingProfile":{"platforms":["MT5_REAL", "MT5_REAL2"]}
+    }
+    FinishResult = falseurl(data)
+    return FinishResult
+#############################################################################
+def request_without_platforms():
+    allure.description("No parameter platforms on request")
+    data = {
+        "tradingProfile":{"name":"Default"}
+    }
+    FinishResult = falseurl(data)
+    return FinishResult
+#############################################################################
+#Проверка параметра Name
+#############################################################################
+def no_value_to_name():
+    allure.description("No value to name")
+    data = {
+        "tradingProfile":{
+            "name":"",
+            "platforms":["MT5_REAL", "MT5_REAL2"]
+        }
+    }
+    FinishResult = falseurl(data)
+    return FinishResult
+#############################################################################
+def value_to_name_not_str():
+    allure.description("Value to name not str")
+    data = {
+        "tradingProfile":{
+            "name":123456,
+            "platforms":["MT5_REAL", "MT5_REAL2"]
+        }
+    }
+    FinishResult = falseurl(data)
+    return FinishResult
+#############################################################################
+def correct_value_to_name():
+    allure.description("Correct value to name")
+    data = {
+        "tradingProfile":{
+            "name":"Default",
+            "platforms":["MT5_REAL", "MT5_REAL2"]
+        }
+    }
+    FinishResult = falseurl(data)
+    return FinishResult
+#############################################################################
+def not_found_profile_for_name_value():
+    allure.description("Not found profile for name value")
+    data = {
+        "tradingProfile":{
+            "name":"for test",
+            "platforms":["MT5_REAL", "MT5_REAL2"]
+        }
+    }
+    FinishResult = falseurl(data)
+    return FinishResult
+#############################################################################
+#Проверка параметра platforms
+#############################################################################
+def no_value_to_platforms():
+    allure.description("No value to platforms")
+    data = {
+        "tradingProfile":{
+            "name":"Default",
+            "platforms":[]
+        }
+    }
+    FinishResult = falseurl(data)
+    return FinishResult
+#############################################################################
+def value_to_platforms_not_str():
+    allure.description("Value to platforms not str")
+    data = {
+        "tradingProfile":{
+            "name":"Default",
+            "platforms":[123456]
+        }
+    }
+    FinishResult = falseurl(data)
+    return FinishResult
+#############################################################################
+def correct_value_to_platforms():
+    allure.description("Correct value to platforms")
+    data = {
+        "tradingProfile":{
+            "name":"Default",
+            "platforms":["MT5_REAL", "MT5_REAL2"]
+        }
+    }
+    FinishResult = falseurl(data)
+    return FinishResult
+#############################################################################
+def not_found_platform_in_profile():
+    allure.description("Not found platform in the profile")
+    data = {
+        "tradingProfile":{
+            "name":"for test",
+            "platforms":["MT5_TEST"]
+        }
+    }
+    FinishResult = falseurl(data)
+    return FinishResult
+#############################################################################
--- Test_ChangeProfile.py
+++ Test_ChangeProfile.py
@@ -8,11 +8,43 @@ def resource_setup(request):
     print(request.scope)
     print(request.cls)
     print('Description: ' +request.function.__name__)
-    #print(request.module.__name__)
-    #print(request.fspath)
     def resource_teardown():
         print("Finish test")
     request.addfinalizer(resource_teardown)
+#############################################################################
+#Проверка запроса ChangeProfile
+#############################################################################
+class Test_ChangeProfile():
+    def test_wrong_request(resource_setup):
+        assert wrong_request() == {'Description': 'invalid uri', 'Status': 3}
+    def test_no_body_request(resource_setup):
+        assert no_body_request() == ''
+    def test_request_without_name_and_platform(resource_setup):
+        assert request_without_name_and_platform() == ''
+    def test_request_without_name(resource_setup):
+        assert request_without_name() == ''
+    def test_request_without_platforms(resource_setup):
+        assert request_without_platforms == ''
+    def test_no_value_to_name(resource_setup):
+        assert no_value_to_name() == ''
+    def test_value_to_name_not_str(resource_setup):
+        assert value_to_name_not_str() == ''
+    def test_correct_value_to_name(resource_setup):
+        assert correct_value_to_name() == ''
+    def test_not_found_profile_for_name_value(resource_setup):
+        assert not_found_profile_for_name_value() == ''
+    def test_no_value_to_platforms(resource_setup):
+        assert no_value_to_platforms() == ''
+    def test_value_to_platforms_not_str(resource_setup):
+        assert value_to_platforms_not_str() == ''
+    def test_correct_value_to_platforms(resource_setup):
+        assert correct_value_to_platforms() == ''
+    def test_not_found_platform_in_profile(resource_setup):
+        assert not_found_platform_in_profile() == ''
 
-def test_wrong_request(resource_setup):
-    assert wrong_request() == {'Description': 'invalid uri', 'Status': 3}
+#############################################################################
+#Проверка запроса Reload
+#############################################################################
+#class Test_Reload():
+#    def test_reload(self):
+#        assert reload() == 'dasrfsdfds'
